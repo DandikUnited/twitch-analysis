7eb7187e9658769f413e90d75e97083c96565088
Erdogan Kerem Akyuz <eka30@itl171.student.eecs.qmul.ac.uk>
2014-11-18 19:10:27 +0000

    Initial Commit

---
+++ src/uk/ac/qmul/bigdata/TwitchAnalysis/RivalMapper.java
@@ -0,0 +1,29 @@
+package uk.ac.qmul.bigdata.TwitchAnalysis;
+
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+public class RivalMapper extends Mapper<Object, Text, Text, IntWritable> { 
+	    private final IntWritable one = new IntWritable(1);
+	    private Text data = new Text();
+	    String dump;
+	    int startIndex;
+	    
+	    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+	    	Date dateObj = new Date();
+	    	dump = value.toString();
+	        if(StringUtils.ordinalIndexOf(dump,";",4)>-1){
+	            startIndex = StringUtils.ordinalIndexOf(dump,"	",2) + 1;
+	            
+	            		//data.set(hashtagarray[i]);
+	            		context.write(data, one);
+	            		}
+	            	}
+	            }
