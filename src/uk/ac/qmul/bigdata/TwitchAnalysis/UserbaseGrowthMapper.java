b710033814d7374d80ff5ec14ed8f33a0ae13186
lucaurso <l.urso@hss14.qmul.ac.uk>
2014-11-20 16:12:16 +0000

    

--- src/uk/ac/qmul/bigdata/TwitchAnalysis/UserbaseGrowthMapper.java
+++ src/uk/ac/qmul/bigdata/TwitchAnalysis/UserbaseGrowthMapper.java
@@ -16,7 +16,7 @@ public class UserbaseGrowthMapper extends Mapper<Object, TwitchDataRecord, Text,
 
 	    public void map(Object key, TwitchDataRecord value, Context context) throws IOException, InterruptedException {
 	    	Text a = new Text();
-	    	String b = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").format(new Date(value.getTimeStamp().get()));
+	    	String b = new SimpleDateFormat("yyyy-MM-dd").format(new Date(value.getTimeStamp().get()));
 	    	a.set(b);
 	    	context.write(a, new IntWritable(1));
 	    }
