fb0b6f0a88b986cf5ee1accaf416ad602d89ab9e
Mustafa Bedi Egilmez <mbe30@itl185.student.eecs.qmul.ac.uk>
2014-12-03 14:58:22 +0000

    "sync with local"

--- src/uk/ac/qmul/bigdata/TwitchAnalysis/Plus18Job.java
+++ src/uk/ac/qmul/bigdata/TwitchAnalysis/Plus18Job.java
@@ -10,6 +10,7 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.StringUtils;
+import org.apache.hadoop.io.NullWritable;
 
 public class Plus18Job {
 		public static void runJob(String[] input, String output) throws Exception {
